6963b8584d05895f359f3db933ee81b72d294ed1
Edson Cepeda <raulcepedac@hotmail.com>
2020-04-13 00:53:22 -0500

    Serve loop added

--- script.js
+++ script.js
@@ -11,98 +11,39 @@ function serve(){
     var x9 = parseInt(document.forms['frm']['h9'].value);
     var counter = parseInt(document.getElementById('counter').value);
     var hop = [x1, x2, x3, x4, x6, x6, x7, x8, x9];
-    var i, minvalue, minhop, food, foodbool, rest,j;
-    
-    console.log(hop);
-
-    if( (x1==0 || x1==null || isNaN(x1)==true) &&
-        (x2==0 || x2==null || isNaN(x2)==true) &&
-        (x3==0 || x3==null || isNaN(x3)==true) &&
-        (x4==0 || x4==null || isNaN(x4)==true) &&
-        (x5==0 || x5==null || isNaN(x5)==true) &&
-        (x6==0 || x6==null || isNaN(x6)==true) &&
-        (x7==0 || x7==null || isNaN(x7)==true) &&
-        (x8==0 || x8==null || isNaN(x8)==true) &&
-        (x9==0 || x9==null || isNaN(x9)==true) ){
-            console.log('Can not serve nutrient paste; all hoppers are empty.');
-    }else if(   (x1>75 || x1<0) ||
-                (x2>75 || x2<0) ||
-                (x3>75 || x3<0) ||
-                (x4>75 || x4<0) ||
-                (x5>75 || x5<0) ||
-                (x6>75 || x6<0) ||
-                (x7>75 || x7<0) ||
-                (x8>75 || x8<0) ||
-                (x9>75 || x9<0)     ){
-            console.log('There are more than 75 food or less than 0 food in some hopper.');
-    }else{
-        do{
-            minvalue = 76;
-            food=0;
-
-            for( i=0; i<9; i++ ){           //Check which hopper has the least amount of food units.
-                if( (hop[i] < minvalue) && (hop[i] != 0) ){ 
-                    minvalue = hop[i];  //Amount of food units that the hopper has.
-                    minhop = i;         //Hopper position (starting from 0) which has the least amount of food units.
-                }
-            }
-
-            console.log('Hopper',minhop+1,'has', minvalue,'food');
-
-            rest = 6 - food;    //Compare how many food units needs to serve a plate of nutrient paste.
-
-            console.log('· Rest:',rest);
-
-            do{
-                if( minvalue > 0 ){
-                    minvalue--;
-                    console.log('· Now has:',minvalue, 'food');
-                    switch(minhop){
-                        case 0:
-                            document.getElementById('h1').value = minvalue;
-                        break;
-                        case 1:
-                            document.getElementById('h2').value = minvalue;
-                        break;
-                        case 2:
-                            document.getElementById('h3').value = minvalue;
-                        break;
-                        case 3:
-                            document.getElementById('h4').value = minvalue;
-                        break;
-                        case 4:
-                            document.getElementById('h5').value = minvalue;
-                        break;
-                        case 5:
-                            document.getElementById('h6').value = minvalue;
-                        break;
-                        case 6:
-                            document.getElementById('h7').value = minvalue;
-                        break;
-                        case 7:
-                            document.getElementById('h8').value = minvalue;
-                        break;
-                        case 8:
-                            document.getElementById('h9').value = minvalue;
-                        break;
-
-                        default:
-                            console.log('switch default');
-                        break;
-                    }
-                    food++;
-                    console.log('· Food:', food);
-                }
-            }while(rest>0 && minvalue>0 && food <6);
-
-            if( food == 6 ){
-                rest = 0;
-                console.log('Nutrient paste served.');
-                document.getElementById('counter').value = counter+1;
-            }
-            console.log('');
-        }while( food == 6 );
-        
+    sum = 0
+    hop.forEach(element => {
+        sum += element;
+    });
+    if(sum < 6) return;
+    console.log(`Initial array: [${hop}]`);
+    let min = getMin(hop);
+    let serve = 6;
+    while(serve  > 0) {
+        if(hop[min] > 0) {
+            hop[min] = hop[min] - 1;
+            serve = serve - 1;
+        } else {
+            min = getMin(hop);
+        }
     }
-    console.log(' ');
+    console.log(`Final array:   [${hop}]`);
+    
+    for(var i=0; i<hop.length; i++) {
+        document.getElementById(`h${i+1}`).value = hop[i];
+    }
+    plates = parseInt(document.getElementById("counter").value);
+    document.getElementById("counter").value = plates + 1;
+}
+
+function getMin(array) {
+    var comp_min = 75;
+    var min = null;
+    for(var i=0; i<array.length; i++) {
+        if(array[i] < comp_min && array[i] != 0) {
+            min = i;
+            comp_min = array[i];
+        }
+    }
+    return min;
 }
